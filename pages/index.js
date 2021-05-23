d3f339ccf4b89402fa1f70dbd87fd68f974a1fd0
Pierre Husson <hussonpierre3@gmail.com>
2021-05-24 01:52:31 +0200

    Finish the body of the frontpage

--- pages/index.js
+++ pages/index.js
@@ -32,18 +32,23 @@ export default function Home() {
 
 
       {/*Body*/}
-      <form>
+      <form className="flex flex-col items-center mt-44 flex-grow">
         <Image
           src='https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'
           height={100}
           width={300}
         />
-        <div className="flex w-full mt-5 hover:shadow">
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500"/>
-          <input type="text" className="focus:outline-none" />
+          <input type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className="h-5"/>
         </div>
 
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Recherche Google</button>
+
+          <button className="btn">J'ai de la chance</button>
+        </div>
       </form>
 
 
