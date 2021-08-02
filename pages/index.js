1d563ed21e8edb3e8aafe5b95476945dcba5b725
Pierre <59602579+pierrehusson23@users.noreply.github.com>
2021-08-02 16:47:28 +0200

    Frontpage Footer and Search engine routing

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,81 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Avatar from '../components/Avatar'
-import {MicrophoneIcon, SearchIcon, ViewGridIcon} from "@heroicons/react/solid"
+import Head from "next/head";
+import Image from "next/image";
+import Avatar from "../components/Avatar";
+import Footer from "../components/Footer";
+import {
+  MicrophoneIcon,
+  SearchIcon,
+  ViewGridIcon
+} from "@heroicons/react/solid";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>Google Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       {/*Header*/}
-      <header className='flex w-full p-5 justify-between text-sm text-gray-700'>
-        <div className='flex space-x-4 items-center'>
-          <p className='link'>About</p>
-          <p className='link'>Store</p>
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
         </div>
 
-        <div className='flex space-x-4 items-center'>
-          <p className='link'>Gmail</p>
-          <p className='link'>Images</p>
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
 
-        <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"/>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
-        <Avatar url='https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg'/>
-          
+          <Avatar url="https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg" />
         </div>
       </header>
 
-
-
       {/*Body*/}
-      <form className="flex flex-col items-center mt-44 flex-grow">
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
         <Image
-          src='https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'
+          src="https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
           height={100}
           width={300}
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
-          <SearchIcon className="h-5 mr-3 text-gray-500"/>
-          <input type="text" className="flex-grow focus:outline-none" />
-          <MicrophoneIcon className="h-5"/>
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
+          />
+          <MicrophoneIcon className="h-5" />
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Recherche Google</button>
+          <button onClick={search} className="btn">
+            Recherche Google
+          </button>
 
-          <button className="btn">J'ai de la chance</button>
+          <button onClick={search} className="btn">
+            J'ai de la chance
+          </button>
         </div>
       </form>
 
-
-
       {/*Footer*/}
-
-
-
+      <Footer />
     </div>
-  )
+  );
 }
